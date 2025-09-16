b73584e2c2b798d117aef63916ad1423a6bd5971
boscot553 <yizhengtian2011@gmail.com>
2025-09-16 10:16:27 -0400

    Create test.py

---
+++ test.py/test.py
@@ -0,0 +1,39 @@
+# Auto detect text files and perform LF normalization
+# x = 3
+# y = float(3)
+# print(x,y)
+
+# values = [1,2.23,5,7,2,30,15]
+# print(values[0])
+# print(values[6])
+# for i in values:
+#     print(i)
+
+# x = "this is a thing"
+# y= x.split( )
+# z = y[0]
+# print(y)
+# print(z)
+
+
+# temp = 75
+# if temp > 68:
+#     print('warm')
+# elif temp == 68:
+#     print('perfect')
+# else:
+#     print('cold')
+
+
+
+# x = input("Gimme a number ")
+# x = int(x)
+# if x % 2 == 0:
+#     print("even")
+# else:
+#     print("odd")
+
+bill = input("How much was your bill?")
+tip = input("Select a tip amount. 0, 15, 20, or 25")
+bill = bill * 0.15
+print(bill)
